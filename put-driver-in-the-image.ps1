5b7229d37a96ededb42544a81deaafa8eefa0f26
Silvio Knizek <killermoehre@gmx.net>
2023-12-28 15:55:06 +0100

    First try checking out the windows runner

---
+++ put-driver-in-the-image.ps1
@@ -0,0 +1,17 @@
+Write-Output "Mounting the ISOs"
+$windowsImageMount = Mount-DiskImage -ImagePath "$PWD\SERVER_EVAL_x64FRE_en-us.iso" -PassThru
+$windowsImageDriveLetter = ($windowsImageMount | Get-Volume).DriveLetter
+$virtioImageMount = Mount-DiskImage -ImagePath "$PWD\virtio-win-0.1.240.iso" -PassThru
+$virtioImageDriveLetter = ($virtioImageMount | Get-Volume).DriveLetter
+
+$installWim = "$windowsImageDriveLetter\sources\install.wim"
+
+Write-Output "Collecting Drivers to Install"
+$drivers = Get-ChildItem -Recurse -Path "$virtioImageDriveLetter\*\2k22\amd64" -Include *.inf
+
+# get the images in the .wim to patch all of them
+$imagesInImage = Get-WindowsImage -ImagePath $installWim
+
+foreach ($image in $imagesInImage) {
+  Convert-WindowsImage -SourcePath $installWim -DiskLayout UEFI -Edition $image.name -Drivers $drivers.FullName
+}
