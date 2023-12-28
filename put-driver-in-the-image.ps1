9642faf1b77f3e342f3a2ff4e8d0003af2e4b046
Silvio Knizek <killermoehre@gmx.net>
2023-12-28 17:53:56 +0100

    Fixing path specifiers

--- put-driver-in-the-image.ps1
+++ put-driver-in-the-image.ps1
@@ -1,13 +1,13 @@
 Write-Output "Mounting the ISOs"
-$windowsImageMount = Mount-DiskImage -ImagePath "$PWD\SERVER_EVAL_x64FRE_en-us.iso" -PassThru
+$windowsImageMount = Mount-DiskImage -ImagePath SERVER_EVAL_x64FRE_en-us.iso -PassThru
 $windowsImageDriveLetter = ($windowsImageMount | Get-Volume).DriveLetter
-$virtioImageMount = Mount-DiskImage -ImagePath "$PWD\virtio-win-0.1.240.iso" -PassThru
+$virtioImageMount = Mount-DiskImage -ImagePath virtio-win-0.1.240.iso -PassThru
 $virtioImageDriveLetter = ($virtioImageMount | Get-Volume).DriveLetter
 
-$installWim = "$windowsImageDriveLetter\sources\install.wim"
+$installWim = Join-Path -Path $windowsImageDriveLetter -ChildPath "\sources\install.wim"
 
 Write-Output "Collecting Drivers to Install"
-$drivers = Get-ChildItem -Recurse -Path "$virtioImageDriveLetter\*\2k22\amd64" -Include *.inf
+$drivers = Get-ChildItem -Recurse -Path $virtioImageDriveLetter\*\2k22\amd64 -Include *.inf
 
 # get the images in the .wim to patch all of them
 $imagesInImage = Get-WindowsImage -ImagePath $installWim
